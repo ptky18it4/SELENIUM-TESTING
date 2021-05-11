5aa17fb635252e6a3210639ad58a6f0e4471e6eb
Trung Ky <phamtrungky14022000@gmail.com>
2021-05-11 09:31:05 +0700

    setup enviroment variable chromedriver

---
+++ Selenium Practice/src/test/java/base/BaseTest.java
@@ -0,0 +1,27 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+class BaseTests {
+    private WebDriver driver;
+
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+    }
+    public void setDriver() {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setDriver();
+    }
+}
+
