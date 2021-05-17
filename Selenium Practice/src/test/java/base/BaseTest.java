415addb8f3cdc7bf69caa8d08c23b765bc2baebf
Trung Ky <phamtrungky14022000@gmail.com>
2021-05-18 00:41:19 +0700

    done link 3

--- Selenium Practice/src/test/java/base/BaseTest.java
+++ Selenium Practice/src/test/java/base/BaseTest.java
@@ -2,11 +2,9 @@ package base;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 class BaseTests {
@@ -15,54 +13,51 @@ class BaseTests {
     public void setUp() throws InterruptedException {
         //System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://testpages.herokuapp.com/styled/basic-html-form-test.html");
+        driver.get("https://www.seleniumeasy.com/test/input-form-demo.html");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
 
-        // TODO: 5/17/2021 : Username
-        driver.findElement(By.name("username")).sendKeys("D");
+        // TODO: 5/17/2021 : First Name
+        driver.findElement(By.name("first_name")).sendKeys("Alex");
 
-        // TODO: 5/17/2021 : Password
-        driver.findElement(By.name("password")).sendKeys("TuiLaPassword");
+        // TODO: 5/17/2021 : Last Name
+        driver.findElement(By.name("last_name")).sendKeys("TuiLaPassword");
 
-        // TODO: 5/17/2021 : TextArea Comment
-        driver.findElement(By.name("comments")).sendKeys("Lorem ipsum....");
+        // TODO: 5/17/2021 : E-Mail
+        driver.findElement(By.name("email")).sendKeys("phamtrungky@gmail.com");
 
-        // TODO: 5/17/2021 : Choose file
-        driver.findElement(By.name("filename")).sendKeys("E:\\DOCUMENTS\\N3 - DOCUMENTS\\SOFTWARE TESTING\\PROJECT TESTING\\Selenium Practice\\resources\\vungro.jpg");
+        // TODO: 5/17/2021 : Phone #
+        driver.findElement(By.name("phone")).sendKeys("8888888888");
         //can use By cssSelector or name (path) as per convenience
 
-        // TODO: 5/17/2021 : Check box Items
-        if (!driver.findElement(By.name("checkboxes[]")).isSelected()) {
-            driver.findElement(By.xpath("//tbody/tr[5]/td[1]/input[1]")).click();
-            Thread.sleep(10000);
-        }
+        // TODO: 5/17/2021 : Address
+        driver.findElement(By.name("address")).sendKeys("Phu Yen - Viet Nam");
 
-        // TODO: 5/17/2021 : Radio Items
-        if (!driver.findElement(By.name("radioval")).isSelected()) {
-            driver.findElement(By.name("radioval")).click();
-            Thread.sleep(10000);
-        }
+        // TODO: 5/17/2021 : City
+        driver.findElement(By.name("city")).sendKeys("Tuy Hoa");
 
-        // TODO: 5/17/2021 : Multi select value
-        List<WebElement> optionSelected = Collections.singletonList(driver.findElement(By.name("multipleselect[]")));
-        for (WebElement option : optionSelected) {
-            System.out.println("Option: " +  option);
-            option.click();
-        }
+        // TODO: 5/17/2021 : State
+        Select optionDropdown = new Select(driver.findElement(By.name("state")));
+        optionDropdown.getOptions().get(3).click();
+        System.out.println("Dropdown: " +  optionDropdown.getOptions().get(3));
 
-        // TODO: 5/17/2021 : Dropdown
-        List<WebElement> optionDropdown = Collections.singletonList(driver.findElement(By.name("dropdown")));
-        for (WebElement option : optionDropdown) {
-            System.out.println("Dropdown: " +  option);
-            option.click();
-        }
+        // TODO: 5/17/2021 : Zip Code
+        driver.findElement(By.name("zip")).sendKeys("54645");
 
-        // TODO: 5/17/2021 : Cancel button
-//        driver.findElement(By.xpath("//tbody/tr[9]/td[1]/input[1]")).click();
+
+        // TODO: 5/17/2021 : Website or domain name
+        driver.findElement(By.name("website")).sendKeys("www.google.com");
+
+        // TODO: 5/17/2021 : Do you have hosting?;
+        if (!driver.findElement(By.name("hosting")).isSelected()) {
+            driver.findElement(By.name("hosting")).click();
+            Thread.sleep(2000);
+        }
+        // TODO: 5/17/2021 : Project Description
+        driver.findElement(By.name("comment")).sendKeys("Tui la comment ne");
+
         // TODO: 5/17/2021 :  Submit submit
-        driver.findElement(By.xpath("//tbody/tr[9]/td[1]/input[2]")).click();
-
+        driver.findElement(By.xpath("//body/div[2]/div[1]/div[2]/section[1]/form[1]/fieldset[1]/div[13]/div[1]/button[1]")).click();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //        driver.quit();
 
