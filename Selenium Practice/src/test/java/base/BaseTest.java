fa582d54eaff808db118d7864a87245d209ab882
Trung Ky <phamtrungky14022000@gmail.com>
2021-05-17 21:53:11 +0700

    done lab-3 link 1

--- Selenium Practice/src/test/java/base/BaseTest.java
+++ Selenium Practice/src/test/java/base/BaseTest.java
@@ -1,48 +1,94 @@
 package base;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
 class BaseTests {
     private WebDriver driver;
 
-    public void setUp() {
+    public void setUp() throws InterruptedException {
         //System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
 
+        // TODO: 5/17/2021 : First name
         driver.findElement(By.id("firstName")).sendKeys("D");
+
+        // TODO: 5/17/2021 : Lastname
         driver.findElement(By.id("lastName")).sendKeys("Kenneth");
+
+        // TODO: 5/17/2021 : Email
         driver.findElement(By.id("userEmail")).sendKeys("phamtrungky@gmail.com");
-        driver.findElement(By.xpath("//label[contains(text(),'Male')]")).click();
+
+        // TODO: 5/17/2021 : Gender
+        //driver.findElement(By.xpath("//label[contains(text(),'Male')]")).click();
+        if (!driver.findElement(By.id("genterWrapper")).isSelected()) {
+            driver.findElement(By.id("genterWrapper")).click();
+        }
+
+        // TODO: 5/17/2021 : Phone number
         driver.findElement(By.id("userNumber")).sendKeys("0909099900");
-        driver.findElement(By.id("dateOfBirthInput")).findElement(By.xpath("//input[@id='dateOfBirthInput']")).click();
 
-        //Selecting the first checkbox using XPath
-        driver.findElement(By.xpath("//label[text()='Sports']")).click();
+        // TODO: 5/17/2021 : Date of birth
+        driver.findElement(By.id("dateOfBirthInput")).findElement(By.xpath("//input[@id='dateOfBirthInput']"));
 
-        //Selecting the second checkbox using Xpath
-        driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/form[1]/div[7]/div[2]/div[1]")).click();
+        // TODO: 5/17/2021 : Subject wrapper
 
+        WebElement autoSubjects = driver.findElement(By.id("subjectsInput"));
+        autoSubjects.sendKeys("en");
+        autoSubjects.sendKeys(Keys.ENTER);
+
+        /*List<WebElement> optionsToSelect = driver.findElements(By.xpath("//input[@id='subjectsInput']"));
+
+        for(WebElement option : optionsToSelect){
+            System.out.println(option);
+            String textToSelect = "English";
+            if(option.getText().equals(textToSelect)) {
+                option.sendKeys(Keys.ENTER);
+                break;
+            }
+        }*/
+
+        // TODO: 5/17/2021 : Hobbies Checkbox
+        if (!driver.findElement(By.id("hobbiesWrapper")).isSelected())
+            driver.findElement(By.id("hobbiesWrapper")).click();
+
+        // TODO: 5/17/2021 : Upload picture
         driver.findElement(By.id("uploadPicture")).sendKeys("E:\\DOCUMENTS\\N3 - DOCUMENTS\\SOFTWARE TESTING\\PROJECT TESTING\\Selenium Practice\\resources");
-        Select state = new Select(driver.findElement(By.name("country")));
-        state.selectByVisibleText("NRC");
+
+        // TODO: 5/17/2021 : Address
+        driver.findElement(By.id("currentAddress")).sendKeys("Phu Yen");
+
+        // TODO: 5/17/2021 : State and City
+/*        Select selectMyElement = new Select(driver.findElement(By.tagName("div")));
+        selectMyElement.selectByVisibleText("NCR");*/
+
+        Actions keyDown = new Actions(driver);
+        keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.DOWN)).perform();
+
+        // TODO: 5/17/2021 : Dropdown : chưa làm được 
+        driver.findElement(By.id("stateCity-wrapper")).findElement(By.id("state")).findElements(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[2]/div[1]/form[1]/div[10]/div[2]/div[1]/div[1]/div[1]"));
+
+
+        // TODO: 5/17/2021 :  Submit
         driver.findElement(By.id("submit")).click();
 
 
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.quit();
+//        driver.quit();
 
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BaseTests test = new BaseTests();
         test.setUp();
     }
