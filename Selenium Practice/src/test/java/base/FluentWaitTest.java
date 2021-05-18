2d26c45db61ad325a3ddc896cee74c9fdb2f133b
Trung Ky <phamtrungky14022000@gmail.com>
2021-05-18 07:50:30 +0700

    done tutorial

--- Selenium Practice/src/test/java/base/FluentWaitTest.java
+++ Selenium Practice/src/test/java/base/FluentWaitTest.java
@@ -1,35 +1,32 @@
 package base;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
 
 import java.util.concurrent.TimeUnit;
 
-class BaseTests {
+class FluentWaitTest {
     private WebDriver driver;
 
     private void setUp() {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
         driver.manage().window().maximize();
-
         driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
-        WebDriverWait wait = new WebDriverWait(driver,20);
-        final Boolean loading = wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.id("loading"))));
-       if (loading) {
-           System.out.println(driver.findElement(By.id("finish")).getText());
-           driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.quit();
-       }
-
+        FluentWait wait = new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS)
+                .pollingEvery(2, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.id("loading"))));
+        System.out.println(driver.findElement(By.id("finish")).getText());
     }
+
 
     public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
+        FluentWaitTest fluentWait = new FluentWaitTest();
+        fluentWait.setUp();
     }
 }
-
