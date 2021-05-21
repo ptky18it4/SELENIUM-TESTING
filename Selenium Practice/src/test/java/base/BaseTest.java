1888a0ee0d9e329470b9b7340d7b35766d4ef785
Trung Ky <phamtrungky14022000@gmail.com>
2021-05-21 22:02:32 +0700

    done demo with junit 5

--- Selenium Practice/src/test/java/base/BaseTest.java
+++ Selenium Practice/src/test/java/base/BaseTest.java
@@ -1,12 +1,7 @@
 package base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 class BaseTests {
     private WebDriver driver;
@@ -15,16 +10,6 @@ class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
         driver.manage().window().maximize();
-
-        driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
-        WebDriverWait wait = new WebDriverWait(driver,20);
-        final Boolean loading = wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.id("loading"))));
-       if (loading) {
-           System.out.println(driver.findElement(By.id("finish")).getText());
-           driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.quit();
-       }
-
     }
 
     public static void main(String[] args) {
